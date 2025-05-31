1bd584b2fe40bac1721d3c2df3b83ccfee9ca8fa
Sai Manogna Sri <saimanognasri.k@gmail.com>
2025-05-31 11:23:43 +0530

    Update qrcode.py

--- qrcode.py
+++ qrcode.py
@@ -1,13 +1,9 @@
 import qrcode
-
-# Define the URL
-data = "https://example.com"  # Replace with your URL
-
+data = "https://manognakatari.github.io/random-quote-generator/" 
 # Create QR code
 qr = qrcode.QRCode(version=1, box_size=10, border=5)
 qr.add_data(data)
 qr.make(fit=True)
-
 # Generate and save the image
 img = qr.make_image(fill="black", back_color="white")
 img.save("my_qr_code.png")
